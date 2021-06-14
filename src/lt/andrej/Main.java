98ee44eb294ac2834b2d6b3e1fcc8f6996b03bfe
amplifiermkiv <andrej.svaikovskij@gmail.com>
2021-06-14 21:00:14 +0300

    comment

---
+++ src/lt/andrej/Main.java
@@ -0,0 +1,19 @@
+package lt.andrej;
+
+import static java.lang.Math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        double x1 = 2;
+        double y1 = 15;
+        double x2 = 9;
+        double y2 = 10;
+
+        double sDiff = pow((x2 - x1), 2);
+        double yDiff = pow((y2 - y1), 2);
+
+        double result = sqrt(sDiff + yDiff);
+        System.out.println(result);
+
+    }
+}
